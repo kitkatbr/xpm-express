5e1a6634b8fa404c2791d3b1d80911d902628b12
kitkatbr <91047095+kitkatbr@users.noreply.github.com>
2024-07-25 23:18:41 -0300

    update

--- src/components/product-selection/product-display/index.js
+++ src/components/product-selection/product-display/index.js
@@ -3,21 +3,33 @@ import React from "react"
 import Info from "./info"
 import OptionSelector from "./option-selector"
 
-const ProductDisplay = ({ region, product }) => {
+
+const ProductDisplay = ({ region, product, cart }) => {
   return product ? (
     <Flex sx={{ flexDirection: "column" }}>
       <Flex sx={{ flexDirection: "row", width: "100%", height: "100%" }}>
+
+              
+
+
+    
         <Image
           sx={{
-            width: "50%",
-            borderRadius: "4px",
+            width: "100%",
+            borderStartEndRadius: "10px",
+            borderStartStartRadius: "10px",
             objectFit: "contain",
             objectPosition: "center center",
+            clipPath: "polygon(0 0, 0 100%, 100% 75%, 100% 0%)",
+            boxShadow: "0px 0px 5px 2px rgba(0, 0, 0, 0.1)",
+
           }}
-          src={product.thumbnail}
+          src={"/elden-ring.webp"}
           alt={product.title}
         />
-        <Info product={product} region={region} />
+
+        
+
       </Flex>
       <Text
         sx={{
@@ -29,9 +41,21 @@ const ProductDisplay = ({ region, product }) => {
         }}
         variant="fz_s"
       >
-        {product.description}
-      </Text>
-      <OptionSelector product={product} />
+    <div className="product-desc">
+      <span className="product-title">
+      Elden Ring <b>Runes</b>
+
+              <span className="badge">
+                New
+              </span>
+      </span>
+      <span className="product-caption">
+      {product.description}
+            </span>
+   
+      
+    </div>      </Text>
+      <OptionSelector product={product} region={region} cart={cart}/>
     </Flex>
   ) : null
 }
